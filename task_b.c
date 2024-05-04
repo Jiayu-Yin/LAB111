5b9a9d737316187449c8cb468929702e9271b4f7
Jiayu-Yin <159534253+Jiayu-Yin@users.noreply.github.com>
2024-05-04 16:57:50 +0100

    Add files via upload

---
+++ task_b.c
@@ -0,0 +1,18 @@
+#define _USE_MATH_DEFINES 
+#include <stdio.h>
+#include <math.h> 
+int main(void){
+    double height = 0;
+	double volume = 0;
+	printf("Enter height: ");
+	scanf("%lf", &height);
+	
+	
+	// Your code below here
+	double radius = 2.2; // Define radius
+	volume = M_PI * pow(radius, 2) * height; // Calculate the volume
+	
+	// Do not edit below here
+	printf("Volume: %.2f", volume);
+	return 0;
+}
