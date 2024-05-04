52e99e740393ea2f63f19de5140c309eccddc672
Jiayu-Yin <159534253+Jiayu-Yin@users.noreply.github.com>
2024-05-04 17:29:04 +0100

    Add files via upload

--- task_e.c
+++ task_e.c
@@ -2,22 +2,27 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    // Check if the number of command-line arguments is not equal to 2
     if (argc != 2) {
         printf("Error. Expected 1 filename.\n");
         return 1;
     }
 
+    // Attempt to open the file
     FILE *fptr = fopen(argv[1], "r");
 
+    // Check if the file was successfully opened
     if (fptr == NULL) {
         printf("Error. Cannot open requested file.\n");
         return 1;
     }
 
+    // Read number from file
     double num;
     fscanf(fptr, "%lf", &num);
     fclose(fptr);
 
+    // Print JSON output
     printf("{\n");
     printf("  \"name\": \"Jiayu Yin\",\n");
     printf("  \"email\": \"jiayu.yin@student.manchester.ac.uk\",\n");
