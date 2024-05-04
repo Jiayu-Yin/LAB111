494d2f08e7ac28eda951e64d42cc7da169c2022e
Jiayu-Yin <159534253+Jiayu-Yin@users.noreply.github.com>
2024-05-04 17:21:29 +0100

    Add files via upload

---
+++ task_e.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Error. Expected 1 filename.\n");
+        return 1;
+    }
+
+    FILE *fptr = fopen(argv[1], "r");
+
+    if (fptr == NULL) {
+        printf("Error. Cannot open requested file.\n");
+        return 1;
+    }
+
+    double num;
+    fscanf(fptr, "%lf", &num);
+    fclose(fptr);
+
+    printf("{\n");
+    printf("  \"name\": \"Jiayu Yin\",\n");
+    printf("  \"email\": \"jiayu.yin@student.manchester.ac.uk\",\n");
+    printf("  \"number1\": %.2f,\n", num);  // Print num formatted to 2 decimal places
+    printf("  \"number2\": %.3e\n", num);  // Print num in scientific notation with 3 digits after the decimal
+    printf("}\n");
+
+    return 0;
+}
