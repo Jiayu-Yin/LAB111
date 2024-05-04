597095f9b2e1dd47f3fcc5035623f46a609f61dc
Jiayu-Yin <159534253+Jiayu-Yin@users.noreply.github.com>
2024-05-04 18:18:58 +0100

    Add files via upload

---
+++ task_h.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+
+    // Get user input
+    #define N 256
+    char buffer[N+1], *filename = buffer;
+    char ch;
+    int i = 0; // counter for how many characters read
+    printf("Enter a file name: ");
+    while ((ch = getchar()) != '\n') { // get one character at a time
+        filename[i++] = ch;
+        // Enter your code under here
+
+        
+    }
+    for (; i < N+1; i++) { // fill the rest of the array with null characters
+        filename[i] = '\0';
+    }
+
+    // Enter your code under here
+    char *ext = strrchr(filename, '.');
+    if (ext != NULL && *(ext + 1) != '\0') { 
+        printf("%s", ext);
+    } else {
+        printf("Error!");
+    }
+
+
+
+
+    return EXIT_SUCCESS;
+}
